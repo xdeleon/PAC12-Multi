b3ac0ff17556b7765faf339529bcd54e30d62895
Xavier De Leon <xdeleon@mybambu.com>
2021-08-19 20:20:08 -0600

    Initial Commit

---
+++ PAC-12 Multi/PAC_12_MultiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PAC_12_MultiApp.swift
+//  PAC-12 Multi
+//
+//  Created by Xavier De Leon on 8/19/21.
+//
+
+import SwiftUI
+
+@main
+struct PAC_12_MultiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
